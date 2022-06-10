a2bcbc7a66fdaa7ddf02b72f188d67b1868bb892
Fahmida <fahmida49@gmail.com>
2022-06-10 17:54:27 -0400

    we are saving pro master

---
+++ src/main/java/com/My2project/July_Project_today/App.java
@@ -0,0 +1,13 @@
+package com.My2project.July_Project_today;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/My2project/July_Project_today/July_Class.java
@@ -0,0 +1,10 @@
+package com.My2project.July_Project_today;
+
+public class July_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
